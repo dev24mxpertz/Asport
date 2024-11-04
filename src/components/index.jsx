d704e71b96df50e42f287905d6139a488a290caa
dev24mxpertz <dev24.mxpertz@gmail.com>
2024-11-04 17:24:05 +0530

    commit

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -11,30 +11,13 @@ import img4 from "../assests/logo2.png";
 import img5 from "../assests/logo2.png";
 import img6 from "../assests/logo2.png";
 import { Link } from "react-router-dom";
+import Gallery from "./Gallery";
 
 const Index = () => {
   const [loadernumber, setloadernumber] = useState(0);
   const [showHome, setshowHome] = useState(false);
   const scrollRef = useRef(null); // Ref for locomotive scroll container
   const homeRef = useRef(null); // Separate ref for Home component
-
-  const generateRows = () => {
-    const rows = [];
-    const images = [img1, img2, img3, img4, img5, img6]; // Array of imported images
-    for (let i = 0; i < 3; i++) {
-      rows.push(
-        <div className="row" key={i}>
-          <div className="card card-left">
-            <img src={images[2 * i]} alt={`img-${2 * i + 1}`} />
-          </div>
-          <div className="card card-right">
-            <img src={images[2 * i + 1]} alt={`img-${2 * i + 2}`} />
-          </div>
-        </div>
-      );
-    }
-    return rows;
-  };
 
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
@@ -64,11 +47,6 @@ const Index = () => {
         };
       },
       pinType: scrollRef.current.style.transform ? "transform" : "fixed",
-    });
-
-    window.addEventListener("resize", () => {
-      scroll.update();
-      ScrollTrigger.refresh();
     });
 
     const t1 = gsap.timeline();
@@ -194,66 +172,22 @@ const Index = () => {
       scroll.update(); // Update LocomotiveScroll after content load
     });
 
-    const ScrollTriggerSettings = {
-      trigger: ".index-main",
-      scroller: scrollRef.current,
-      start: "top 25%",
-      toggleActions: "play reverse play reverse",
-    };
-
-    const LeftXValues = [-800, -900, -400];
-    const rightXValues = [800, 900, 400];
-    const LeftRotationValues = [-30, -20, -35];
-    const RightRotationValues = [30, 20, 35];
-
-    gsap.utils.toArray(".row").forEach((row, index) => {
-      const cardLeft = row.querySelector(".card-left");
-      const cardRight = row.querySelector(".card-right");
-
-      gsap.to(cardLeft, {
-        x: LeftXValues[index],
-        scrollTrigger: {
-          trigger: ".index-main",
-          scroller: scrollRef.current,
-          start: "top center",
-          end: "150% bottom",
-          scrub: true,
-          onUpdate: (self) => {
-            const progress = self.progress;
-            cardLeft.style.transform = `translateX(${
-              progress * LeftXValues[index]
-            }px) rotate(${progress * LeftRotationValues[index]}deg)`;
-            cardRight.style.transform = `translateX(${
-              progress * rightXValues[index]
-            }px) rotate(${progress * RightRotationValues[index]}deg)`;
-          },
-        },
-      });
+    window.addEventListener("resize", () => {
+      scroll.update();
+      ScrollTrigger.refresh();
     });
 
-    gsap.to(".index-logo", {
-      scale: 1,
-      duration: 0.5,
-      ease: "power1.out",
-      scrollTrigger: ScrollTriggerSettings,
-    });
-
-    gsap.to(".index-line p", {
-      y: 0,
-      stagger: 0.1,
-      duration: 0.5,
-      ease: "power1.out",
-      scrollTrigger: ScrollTriggerSettings,
-    });
-
-    gsap.to("index-btn", {
-      y: 0,
-      opacity: 1,
-      delay: 0.25,
-      duration: 0.5,
-      ease: "power1.out",
-      scrollTrigger: ScrollTriggerSettings,
-    });
+    //  gsap.to('.index-hero' , {
+    //   y:"-100%",
+    //   ease:"none",
+    //   duration:0.3,
+    //   scrollTrigger:{
+    //     trigger:".index_main_container_child_image_div4",
+    //     start:"top top",
+    //     end:"100vh bottom",
+    //     scrub:1
+    //   }
+    //  })
 
     // Clean up on component unmount
     return () => {
@@ -297,42 +231,92 @@ const Index = () => {
           <>
             <div ref={homeRef}>
               <section className="index-hero" data-scroll-section>
-                <div className="index-hero-img">
-                  <img src={img1} alt="img1" />
+                <div className="hero-content-box">
+                  <h1> HII, I'M Aman Sharma</h1>
+                  <h3>FullStack Developer</h3>
+                  <p>
+                    Innovative Frontend & Backend Developer looking for an
+                    opportunity to advance my career with reputated brands.
+                    Expertise in ReactJS, NodeJS, HTML, Css, JavaScript, GSAP,
+                    MongoDB, MySQL, RestFull-API'S, AdobeXD, Figma, UI/UX
+                    Development & Designing, ThreeJS and many More.
+                  </p>
+
+                  <div className="dividediv">
+                    <p>Age - 24</p>
+                    <p>Nationallity - Indian</p>
+                  </div>
+
+                  <div className="dividediv">
+                    <p>Gender - Male</p>
+                    <p>Language - Hindi , English</p>
+                  </div>
+                  <div className="dividediv">
+                    <p>Phone - +916263571539</p>
+                    <p>Email - amansharma1503@gmail.com</p>
+                  </div>
+                  <div className="socialdiv">
+                    <Link
+                      style={{ textDecoration: "none" }}
+                      target="_blank"
+                      to="https://www.linkedin.com/in/amansharma1503/"
+                    >
+                      <button className="btn btn-outline-success">
+                        <i className="bi bi-linkedin"></i>
+                      </button>
+                    </Link>
+                    <Link
+                      style={{ textDecoration: "none" }}
+                      target="_blank"
+                      to="https://github.com/Amansh23"
+                    >
+                      <button className="btn btn-outline-success">
+                        <i className="bi bi-github"></i>
+                      </button>
+                    </Link>
+                    <Link
+                      style={{ textDecoration: "none" }}
+                      target="_blank"
+                      to="mail:amansharma1503@gmail.com"
+                    >
+                      <button className="btn btn-outline-success">
+                        <i className="bi bi-envelope-at-fill"></i>
+                      </button>
+                    </Link>
+                    <Link
+                      style={{ textDecoration: "none" }}
+                      target="_blank"
+                      to="https://twitter.com/aman_sharma2309"
+                    >
+                      <button className="btn btn-outline-success">
+                        <i className="bi bi-twitter-x"></i>
+                      </button>
+                    </Link>
+
+                    <Link
+                      style={{ textDecoration: "none" }}
+                      target="_blank"
+                      to="https://www.instagram.com/_aman_2309/"
+                    >
+                      <button className="btn btn-outline-success">
+                        <i className="bi bi-instagram"></i>
+                      </button>
+                    </Link>
+                    <Link
+                      style={{ textDecoration: "none" }}
+                      target="_blank"
+                      to="https://www.facebook.com/people/Aman-Sharma/pfbid0cX3kppbcEfGeeWehkuCxC1aVEZoLXd2o4bVrBdckXnEKH9wBQ72mSCsGUEzpFtWCl/"
+                    >
+                      <button className="btn btn-outline-success">
+                        <i className="bi bi-facebook"></i>
+                      </button>
+                    </Link>
+                  </div>
                 </div>
               </section>
 
               <section className="index-main" data-scroll-section>
-                <div className="index-main-content">
-                  <div className="index-logo">
-                    <img src={img2} alt="img2" />
-                  </div>
-                  <div className="index-copy">
-                    <div className="index-line">
-                      <p>Deliver into coding without clutter.</p>
-                    </div>
-                    <div className="index-line">
-                      <p>Deliver into coding without clutter.</p>
-                    </div>
-                    <div className="index-line">
-                      <p>Deliver into coding without clutter.</p>
-                    </div>
-                  </div>
-                  <div className="index-btn">
-                    <button>Get PRO</button>
-                  </div>
-                </div>
-                {generateRows()}
-              </section>
-
-              <section className="index-footer" data-scroll-section>
-                <Link
-                  className="index-footer-link"
-                  to="codegrid.gumroad.com/l/codegridpro"
-                  target="_blank"
-                >
-                  Link in the Description
-                </Link>
+                {/* <Gallery/> */}
               </section>
             </div>
           </>
