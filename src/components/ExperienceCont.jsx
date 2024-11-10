d291b0d472f280ea6acf55fb81ca5a89071bba33
Aman Sharma <dev24.mxpertz@gmail.com>
2024-11-10 23:07:16 +0530

    10Oct

--- src/components/ExperienceCont.jsx
+++ src/components/ExperienceCont.jsx
@@ -1,12 +1,38 @@
 import React from 'react'
 import "./componentstyle.css"
+import gsap from 'gsap';
+import { SplitText } from 'gsap/SplitText';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 const ExperienceCont = () => {
+  
+  gsap.registerPlugin(SplitText,ScrollTrigger)
+
+
+  let mysplitText  = new SplitText('.split',{type:"chars"})
+  let chars  =  mysplitText.chars
+
+
+  gsap.from(chars , {
+    yPercent:130,
+    stagger:"0.02",
+    ease:"back.out",
+    duration:2,
+    scrollTrigger:{
+      trigger:".split",
+      start:"top 80%",
+      markers:true
+    }
+  })
+
+
+
   return (
     <div className="ExperienceCont_container">
-      <div className="ExperienceCont_Left"></div>
+      {/* <div className="ExperienceCont_Left"></div>0.
       <div className="ExperienceCont_CenterLine"></div>
-      <div className="ExperienceCont_Right"></div>
+      <div className="ExperienceCont_Right"></div> */}
+      <h3 className='split'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente consectetur a, reprehenderit ut id qui, impedit perspiciatis dolore beatae sit, doloremque vero deserunt praesentium doloribus ipsa velit quisquam esse. Beatae?</h3>
     </div>
   );
 }
